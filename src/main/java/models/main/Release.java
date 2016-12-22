84105acf4eaa010f5e804768e013780e93ee2378
Pham Dang Khoa <pdkhoa@tma.com.vn>
2016-12-22 11:52:25 +0700

    fix error

--- src/main/java/models/main/Release.java
+++ src/main/java/models/main/Release.java
@@ -1,12 +1,19 @@
 package models.main;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonValue;
+@JsonIgnoreProperties(ignoreUnknown = true)
 public enum Release {
-    R1_3_0("1.3.0"), R1_2_0("1.2.0"), R1_2_01("1.2.01");
+    R1_3_0("1.3.0"), 
+    R1_2_0("1.2.0"),
+    R1_2_01("1.2.01");
     
     private String value;
     private Release(String str){
         value = str;
     }
+    @JsonCreator
     public Release fromString(String str){
             if(str.contains("1.3.0") || str.contains("130")){
                 return R1_3_0;
@@ -17,7 +24,8 @@ public enum Release {
             }
             return null;
     }
-
+    
+    @JsonValue
     @Override
     public String toString() {
         return value;
